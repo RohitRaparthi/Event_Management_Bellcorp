5c99451ba32ede93da2122b96c278bb272ed9657
RohitRaparthi <rohit.raparthi2003@gmail.com>
2026-02-14 22:19:04 +0530

    API Updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://event-management-bellcorp.onrender.com/api';
 
 export const authAPI = {
   register: async (name, email, password) => {
